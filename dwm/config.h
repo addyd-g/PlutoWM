7cff3e7af7ec2e3509b8c7a9b53809a0200e679e
AdrianDraber <draberadrian@gmail.com>
2022-01-24 18:31:50 +0000

    Fixed top bar

--- dwm/config.h
+++ dwm/config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "nerd-fonts:size=13", "monospace:size=11"};
+static const char *fonts[]          = {  "monospace:size=11", "fontawesome:size=13"};
 static const char dmenufont[]       = "monospace:size=11";
 
 //background colour
@@ -13,11 +13,13 @@ static const char col_gray1[]       = "#29353B";
 //inactive window border
 static const char col_gray2[]       = "#444444";
 //font colour
-static const char col_gray3[]       = "#E67300";
+static const char col_gray3[]       = "#89b482";
 //current window colour and tag colour
-static const char col_gray4[]       = "#EAFFE6";
+static const char col_gray4[]       = "#eeeeee";
 //bar colour
 static const char col_cyan[]        = "#FFAB2E";
+//top bar
+static const char col_orange[]      = "#9b6617";
 
 
 static const char *colors[][3]      = {
--- scripts/bar.sh
+++ scripts/bar.sh
@@ -20,8 +20,8 @@ interval=0
 cpu() {
 	cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
 
-	printf "^c$black^ ^b$green^ CPU"
-	printf "^c$white^ ^b$grey^ $cpu_val"
+	printf "^c$black^ ^b$green^ "
+	printf "^c$black^ ^b$green^ $cpu_val"
 }
 
 mem() {
@@ -29,22 +29,35 @@ mem() {
 	printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
 }
 
+pkg_updates() {
+	# updates=$(doas xbps-install -un | wc -l) # void
+	updates=$(checkupdates-aur | wc -l)   # arch , needs pacman contrib
+	# updates=$(aptitude search '~U' | wc -l)  # apt (ubuntu,debian etc)
+
+	if [ -z "$updates" ]; then
+		printf "^b$black^^c$darkblue^  Fully Updated"
+	else
+		printf "^b$black^^c$blue^  $updates"" updates"
+	fi
+}
+
 wlan() {
 	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "^c$black^ ^b$blue^  ^d^%s" " ^c$blue^Connected" ;;
-	down) printf "^c$black^ ^b$blue^  ^d^%s" " ^c$blue^Disconnected" ;;
+	up) printf "^c$black^ ^b$blue^  Connected" ;;
+	down) printf "^c$black^ ^b$red^  Disconnected" ;;
 	esac
 }
 
 clock() {
-	printf "^c$black^ ^b$darkblue^   "
-	printf "^c$black^^b$blue^ $(date '+%I:%M %p')  "
+	printf "^c$black^ ^b$green^ "
+	printf "^c$black^^b$green^ $(date '+%I:%M %p')  "
 }
+
 
 while true; do
 
-	[ $interval = 0 ] || [ $(($interval % 3600)) = 0 ]
+	[ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
 	interval=$((interval + 1))
 
-	sleep 1 && xsetroot -name "$(cpu) $(mem) $(wlan) $(clock)"
+	sleep 1 && xsetroot -name " $(cpu) $(mem) $(wlan) $updates $(clock)"
 done
